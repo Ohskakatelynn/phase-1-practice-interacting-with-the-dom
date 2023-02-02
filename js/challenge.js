10b46d0ab3abc90fadfbb60134302b85b238b2e8
Scott <scotthenry1@gmail.com>
2023-02-02 12:05:43 -0500

    Pausing and Locking

--- js/challenge.js
+++ js/challenge.js
@@ -8,9 +8,22 @@ function incrementCounter () {
 
 const pause = document.getElementById('pause')
 pause.addEventListener('click', function() {
-    clearInterval(timer)
+    if (pause.textContent === ' pause ') {
+        clearInterval(timer);
+        pause.textContent = 'Resume';
+        document.getElementById('minus').disabled = true;
+        document.getElementById('plus').disabled = true;
+        document.getElementById('heart').disabled = true;
+        document.getElementById('submit').disabled = true;
+    } else if (pause.textContent === 'Resume') {
+        const timer = setInterval( incrementCounter, 1000 )
+        pause.textContent = ' pause ';
+        document.getElementById('minus').disabled = false;
+        document.getElementById('plus').disabled = false;
+        document.getElementById('heart').disabled = false;
+        document.getElementById('submit').disabled = false;
+    }
 })
-
 
 const minus = document.getElementById('minus')
 minus.addEventListener('click',function() {
